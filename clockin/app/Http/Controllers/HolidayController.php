7ed8742f30f9c7d50fe5e56ce57a0d0ded4812ed
NicoReimer <92334801+NicoReimer@users.noreply.github.com>
2024-08-11 19:16:02 +0200

    Update HolidayController.php
    Controller auf den aktuellen Stand gebracht.

--- clockin/app/Http/Controllers/HolidayController.php
+++ clockin/app/Http/Controllers/HolidayController.php
@@ -2,10 +2,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use App\Models\Ticket;
-class HolidayController extends Controller
+class HolidayController extends Controller 
 {
     public function store(Request $request)
     {
@@ -16,24 +15,17 @@ class HolidayController extends Controller
             'reason' => 'required|string|max:255',
         ]);
 
-        if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['startDate']) && isset($_POST['endDate']) && isset($_POST['reason'])) {
-            $startDate = $_POST['startDate'];
-            $endDate = $_POST['endDate'];
-            $reason = $_POST['reason'];
+        // Ticket speichern
+        $userId = auth()->id();
 
-            $userId = auth()->id();
-
-            DB::table('stamps')->insert(
-                array(
-                    'user_id'     =>   $userId,
-                    'start_date'   =>   $startDate,
-                    'end_date'  =>  $endDate,
-                    'reason'    =>  $reason
-                )
-            );
-
-            $successMessage = 'Urlaub beantragt!';
-        }
+        DB::table('tickets')->insert(
+            array(
+                'user_id'     =>   $userId,
+                'start_date'   =>   $startDate,
+                'end_date'  =>  $endDate,
+                'reason'    =>  $reason
+            )
+        );
 
         // Erfolgsnachricht und Weiterleitung
         return redirect()->back()->with('success', 'Der Urlaub wurde erfolgreich beantragt.');
